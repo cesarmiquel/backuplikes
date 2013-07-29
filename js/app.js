3df380fa4ba05de73c30bd6e8b4561fa27ef173c
Cesar Miquel <miquel@easytech.com.ar>
2013-07-29 00:10:12 -0300

    Clean up code. Open with blog given in URL.
    * Now if the URL has a #blog-name we open that blog.
    * Clean up a little the code.

--- js/app.js
+++ js/app.js
@@ -15,12 +15,63 @@ $(window).load(function() {
 	// hide blog info on start
 	$('#blog-info').hide();
 
+  // if the URL specifies a blog show it.
+  var hash = window.location.hash;
+  if (hash != '') {
+    showBlog(hash.replace('#', ''));
+  } 
+
+  // Events ---------------------------------------
 	$('.blog-icon').hover(function() {
 		var blogName = $(this).data('blog');
 		showBlogInfo(blogName);
 	});
 
 	$('.blog-icon').click(function() {
+		var blogName = $(this).data('blog');
+    showBlog(blogName);
+	});
+
+	$(window).resize(function() {
+		// force width
+		$('#main article').width($('#main').width());
+
+		// Run masonry
+		var $container = $('.blog-posts');
+		$container.imagesLoaded(function(){
+			$container.masonry({
+				itemSelector : '.blog-post',
+				columnWidth : 500,
+				isFitWidth: true,
+				containerStyle: {position: 'absolute'},
+			});
+		});
+	});
+
+	// show hide icons
+	$(document).mousemove(function(e) {
+		window.barVisible = window.barVisible == undefined  ? true : window.barVisible;
+		window.barAnimating = window.barAnimating == undefined  ?  false : window.barAnimating;
+		var y = e.pageY;
+		y += $('html').offset().top;
+		if (window.barVisible && y > 90 && !window.barAnimating) {
+			window.barVisible = false;
+			window.barAnimating = true;
+			$('.flexslider').animate({'margin-top': '-=65'}, 250, function() {window.barAnimating = false});
+			// show selected blog
+			if (window.selectedBlog != '') {
+				showBlogInfo(window.selectedBlog);
+			}
+		}
+		else if (window.barVisible == false && y < 20 && window.barAnimating == false) {
+			window.barVisible = true;
+			window.barAnimating = true;
+			$('.flexslider').animate({'margin-top': '+=65'}, 250, function() {window.barAnimating = false});
+		}
+	});
+
+	// helpers
+	function showBlog(blogName) {
     // set opacity on previously selected blog to 1
     if (window.selectedBlog != '') {
       $img = $("[data-blog='" + window.selectedBlog + "'] img");
@@ -29,7 +80,6 @@ $(window).load(function() {
       }
     }
 
-		var blogName = $(this).data('blog');
 		var info = window.blogs[blogName].info;
  
 		var html = '<article class="blog-posts">';
@@ -72,48 +122,8 @@ $(window).load(function() {
     if( $img) {
       $img.css('opacity', 0.3);
     }
+  }
 
-	});
-
-	$(window).resize(function() {
-		// force width
-		$('#main article').width($('#main').width());
-
-		// Run masonry
-		var $container = $('.blog-posts');
-		$container.imagesLoaded(function(){
-			$container.masonry({
-				itemSelector : '.blog-post',
-				columnWidth : 500,
-				isFitWidth: true,
-				containerStyle: {position: 'absolute'},
-			});
-		});
-	});
-
-	// show hide icons
-	$(document).mousemove(function(e) {
-		window.barVisible = window.barVisible == undefined  ? true : window.barVisible;
-		window.barAnimating = window.barAnimating == undefined  ?  false : window.barAnimating;
-		var y = e.pageY;
-		y += $('html').offset().top;
-		if (window.barVisible && y > 90 && !window.barAnimating) {
-			window.barVisible = false;
-			window.barAnimating = true;
-			$('.flexslider').animate({'margin-top': '-=65'}, 250, function() {window.barAnimating = false});
-			// show selected blog
-			if (window.selectedBlog != '') {
-				showBlogInfo(window.selectedBlog);
-			}
-		}
-		else if (window.barVisible == false && y < 20 && window.barAnimating == false) {
-			window.barVisible = true;
-			window.barAnimating = true;
-			$('.flexslider').animate({'margin-top': '+=65'}, 250, function() {window.barAnimating = false});
-		}
-	});
-
-	// helpers
 	function showBlogInfo(blogName) {
 		var info = window.blogs[blogName].info;
 		var numPosts = window.blogs[blogName].posts.length
